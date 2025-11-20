1214ff1e1ea049cc7719f42da19cd1092f44f550
Doyevskyi <ga.miro3721@gmail.com>
2025-11-20 14:50:46 +0900

    refactor: ApplicationPidFileWriter 설정 분리 및 애플리케이션 부트스트랩 클래스 원복
    - PidFileConfig로 ApplicationPidFileWriter 설정 분리 (prod 프로파일 한정)

--- src/main/java/kr/me/seesaw/SeesawApiApplication.java
+++ src/main/java/kr/me/seesaw/SeesawApiApplication.java
@@ -2,16 +2,12 @@ package kr.me.seesaw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class SeesawApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(SeesawApiApplication.class);
-        ApplicationPidFileWriter writer = new ApplicationPidFileWriter();
-        application.addListeners(writer);
-        application.run(args);
+        SpringApplication.run(SeesawApiApplication.class, args);
     }
 
 }
