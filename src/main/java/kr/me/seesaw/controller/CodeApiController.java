5ff6805bc2a03f79982a6ebfa3cbcd294ee16f30
Doyevskyi <ga.miro3721@gmail.com>
2025-12-06 19:08:50 +0900

    refactor: 코드 API 간소화

--- src/main/java/kr/me/seesaw/controller/CodeApiController.java
+++ src/main/java/kr/me/seesaw/controller/CodeApiController.java
@@ -6,11 +6,12 @@ import kr.me.seesaw.service.CodeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
@@ -20,15 +21,12 @@ public class CodeApiController {
     private final CodeService codeService;
 
     @GetMapping
-    public ResponseEntity<List<CodeModel>> getCodes() {
-        List<CodeModel> codes = codeService.get();
-        return ResponseEntity.ok(HierarchicalFactory.build(codes));
-    }
-
-    @GetMapping("/{name}")
-    public ResponseEntity<List<CodeModel>> getCode(@PathVariable("name") String name) {
-        List<CodeModel> codes = codeService.findByName(name);
-        return ResponseEntity.ok(HierarchicalFactory.build(codes));
+    public ResponseEntity<Map<String, List<CodeModel>>> getAllCodes(
+            @RequestParam(name = "name", required = false) String name,
+            @RequestParam(name = "isNested", defaultValue = "false") boolean isNested
+    ) {
+        List<CodeModel> codes = name == null ? codeService.getAllCodes() : codeService.getAllCodesByName(name);
+        return ResponseEntity.ok(Map.of("codes", isNested ? HierarchicalFactory.build(codes) : codes));
     }
 
 }
