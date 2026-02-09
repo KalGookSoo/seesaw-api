1de0caa2f42974213e2e71e6f00e0c856b5aa241
Doyevskyi <ga.miro3721@gmail.com>
2026-02-09 15:00:24 +0900

    feat: 테스트 데이터 초기화 구성에 BasePermission 추가
    - 테스트 환경에서 Security 관련 권한을 다루기 위해 BasePermission 추가

--- src/test/java/kr/me/seesaw/config/TestDataInitializerConfig.java
+++ src/test/java/kr/me/seesaw/config/TestDataInitializerConfig.java
@@ -15,6 +15,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.acls.domain.BasePermission;
 
 import java.util.UUID;
 
