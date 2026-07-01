eac2ee7655a3662b5925b68a7463ee9e1b5e4857
Doyevskyi <ga.miro3721@gmail.com>
2026-07-01 18:04:40 +0900

    chore: 로그 포맷 변경

--- src/main/java/kr/me/seesaw/framework/config/P6SpyCustomFormatter.java
+++ src/main/java/kr/me/seesaw/framework/config/P6SpyCustomFormatter.java
@@ -7,7 +7,7 @@ public class P6SpyCustomFormatter implements MessageFormattingStrategy {
     @Override
     public String formatMessage(int connectionId, String now, long elapsed, String category, String prepared, String sql, String url) {
         if ("statement".equals(category) && sql != null && !sql.trim().isEmpty()) {
-            return sql;
+            return String.format("Executed SQL: %s;", sql);
         }
         return now + " | " + elapsed + "ms | " + category + " | connection " + connectionId + " | " + sql;
     }
